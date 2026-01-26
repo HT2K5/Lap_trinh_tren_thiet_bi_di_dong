2312c47f8dfa7aaef5c3ecc5d51d907dea823202
HT2K5 <cohoaiha@gmail.com>
2026-01-26 11:11:56 +0700

    Initital commit

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={[styles.square, { backgroundColor: 'aqua' }]}>Square 1</Text>
+      <Text style={[styles.square, { backgroundColor: '#008080' }]}>Square 2</Text>
+      <Text style={[styles.square, { backgroundColor: '#ff6666' }]}>Square 3</Text>
     </View>
   );
 }
@@ -13,8 +13,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  square: {
+    width: 100,
+    height: 100,
+    margin: 20,
+    backgroundColor: 'gray',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    color: 'white',
+    fontWeight: 'bold',
+    lineHeight: 100,
   },
 });
